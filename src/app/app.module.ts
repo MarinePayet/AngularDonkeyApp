7582356e7e205d9a648f329efe5f02a6e7fc9c18
Marine PAYET <marine.payet3@gmail.com>
2023-05-25 11:47:51 +0200

    ok bootsratp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateDonkeyComponent } from './create-donkey/create-donkey.component';
+import { ListDonkeyComponent } from './list-donkey/list-donkey.component';
+import { UserDonkeyComponent } from './user-donkey/user-donkey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateDonkeyComponent,
+    ListDonkeyComponent,
+    UserDonkeyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/list-donkey/list-donkey.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-donkey',
+  templateUrl: './list-donkey.component.html',
+  styleUrls: ['./list-donkey.component.css']
+})
+export class ListDonkeyComponent {
+
+}
